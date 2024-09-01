1436a4e3a1ff6061b624d9b46b2ca65cc9bc92bc
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-09-01 21:44:03 +0200

    Update lint.lua

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,12 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'eslint' },
+        typescript = { 'eslint' },
+        typescriptreact = { 'eslint' },
+        javascriptreact = { 'eslint' },
+        zsh = { 'zsh' },
+        sh = { 'shellcheck' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
