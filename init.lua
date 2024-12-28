2bc31bceb7fb90ea0d39810db4d616944a6d82cf
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2024-12-28 20:42:09 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -63,7 +63,7 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
@@ -517,6 +517,15 @@ require('lazy').setup({
         }
       end, { desc = 'toggle diagnostic' })
       vim.keymap.set('n', '<leader>ö', vim.cmd.DiagnosticToggle)
+
+      -- Change diagnostic symbols in the sign column (gutter)
+      if vim.g.have_nerd_font then
+        local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+        for type, icon in pairs(signs) do
+          local hl = 'DiagnosticSign' .. type
+          vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+      end
 
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
