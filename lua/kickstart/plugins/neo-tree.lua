7981a20e09f3675e946204c5e312e93afd01f29e
Johannes Rosengren <98045373+Luxcorel@users.noreply.github.com>
2025-09-25 19:26:18 +0200

    Add easydotnet plugin

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,8 +18,19 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
+          ['R'] = 'easy',
         },
       },
+    },
+    commands = {
+      -- "New file" menu for easydotnet
+      ['easy'] = function(state)
+        local node = state.tree:get_node()
+        local path = node.type == 'directory' and node.path or vim.fs.dirname(node.path)
+        require('easy-dotnet').create_new_item(path, function()
+          require('neo-tree.sources.manager').refresh(state.name)
+        end)
+      end,
     },
   },
 }
